f0f34790bf00724c045655ffa2df6fc7a40852e3
aluisio <aaluisio309@gmail.com>
2024-08-12 22:51:45 -0300

    components without having to reload the website

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+function main(page){
+    const view = document.getElementById('view');
+    fetch(`pages/${page}.html`)
+    .then(response => {
+        if(!response.ok){
+            console.error("NOT fund componete");
+            response.text() = "NOT fund componete";
+        }
+        return response.text();
+    })
+    .then(data => {
+        view.innerHTML = data;
+    })
+    .catch(error => {
+        console.error("not fund:" ,error);
+        view.innerHTML = "NOT fund componete";
+    })
+}
+
+document.addEventListener("DOMContentLoaded", main("home"))
